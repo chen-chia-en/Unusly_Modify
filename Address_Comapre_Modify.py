50896a182ffd891945096ae2aee4794c44aa2426
Molly Chen <87238365+chen-chia-en@users.noreply.github.com>
2022-06-03 10:07:13 +0800

    Update Address_Comapre_Modify.py

--- Address_Comapre_Modify.py
+++ Address_Comapre_Modify.py
@@ -1,8 +1,3 @@
-def Convert(string):
-    list1=[]
-    list1[:0]=string
-    return list1
-
 wrong_address = [
     '555 Fulton St, Unit 520, San Francisco, California',
     '555 Fulton St, Unit 510, San Francisco, California',
@@ -10,25 +5,22 @@ wrong_address = [
 
 ans =  '555 Culton St, Unit 510, San Francisco'.replace(', ','')
 ans = ans.replace(" ",'')
-ans = Convert(ans)
+ans = list(ans)
+# print(ans)
 
 
 trim_address =list()
 for address in wrong_address:
     address = address.replace(', ','')
     address = address.replace(" ",'')
-    trim_address.append(address)
-
-compare_list = list()
-
-for i in trim_address:
-    compare_list.append(Convert(i))
-print(compare_list)
+    trim_address.append(list(address))
+    
+# print(trim_address)
 
 
-for i in range(3):
+for i,value in enumerate(trim_address):
     compare_score = 0
     for x in range(0,len(ans)):
-        if compare_list[i][x] == ans[x]:
+        if trim_address[i][x] == ans[x]:
             compare_score +=1
-    print(str(i+1) + 'choice: ' + str(compare_score))
+    print(str(i+1) + ' choice: ' + str(compare_score))
